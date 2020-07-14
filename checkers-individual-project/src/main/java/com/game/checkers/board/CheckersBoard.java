0b7b2c361e2a5797265e79b8b49aafb33ed75ad0
kozefina <hamonnah@gmail.com>
2020-07-14 20:24:49 +0200

    first commit

---
+++ checkers-individual-project/src/main/java/com/game/checkers/board/CheckersBoard.java
@@ -0,0 +1,24 @@
+package com.game.checkers.board;
+
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+
+public class CheckersBoard {
+
+    BorderPane borderPane = new BorderPane();
+    private final Image imageBoard = new Image(Resources.getPath("board.jpg"));
+
+    public BorderPane getBorderPane() {
+        return borderPane;
+    }
+
+    private Background createBoardBackground() {
+        BackgroundSize backgroundSize = new BackgroundSize(612, 612, false, false, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageBoard, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+        return background;
+    }
+
+
+}
---
+++ checkers-individual-project/src/main/java/com/game/checkers/runner/CheckersRunner.java
@@ -0,0 +1,23 @@
+package com.game.checkers.runner;
+
+import checkers.Board.CheckersBoard;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class CheckersRunner extends Application {
+
+    private static Stage primaryStage = new Stage();
+    private CheckersBoard checkersBoard= new CheckersBoard();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        Scene scene = new Scene(checkersBoard.getBorderPane(), 585, 612, Color.BLACK);
+        primaryStage.setTitle("Checkers Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
